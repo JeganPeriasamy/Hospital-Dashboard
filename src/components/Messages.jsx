42838fd3cfbba81319db3fd5f1c30da4a01dd6ff
JEGAN PERIASAMY <166896131+JeganPeriasamy@users.noreply.github.com>
2024-06-26 20:36:39 +0530

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/message/getall",
+          "https://hospital-back-99kc.onrender.com/api/v1/message/getall",
           { withCredentials: true }
         );
         setMessages(data.messages);
