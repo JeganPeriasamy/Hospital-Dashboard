15bf9ff5b5991e77ddda65acf1b2d48d9ac387a1
JEGAN PERIASAMY <166896131+JeganPeriasamy@users.noreply.github.com>
2024-06-26 20:34:58 +0530

    Update AddNewAdmin.jsx

--- src/components/AddNewAdmin.jsx
+++ src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/admin/addnew",
+          "https://hospital-back-99kc.onrender.com/api/v1/user/admin/addnew",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
